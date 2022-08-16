f81d6ecb90580fd6516614914d5e93a868fa9c1e
Daniel Obisanya <ifeoluwadaniel25@gmail.com>
2022-08-16 11:58:44 +0100

    Started a new project - Jobify

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Logo from "./../assets/images/logo.svg";
 import main from "./../assets/images/main.svg";
+import Wrapper from "./../assets/wrappers/LandingPage";
 
 function Landing() {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={Logo} alt="Jobify" className="logo"></img>
       </nav>
       <div className="container page">
         <div className="info">
           <h1>
-            job <span>tracking</span>
+            job <span>tracking</span> app
           </h1>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -19,10 +20,13 @@ function Landing() {
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </p>
-          <button className="btn btn-hero"></button>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <div>
+          <img src={main} alt="job hunt" className="img main-img"></img>
         </div>
       </div>
-    </main>
+    </Wrapper>
   );
 }
 

62134e969107e69eb74c0ac3330d72ef7aac43f6
Daniel Obisanya <ifeoluwadaniel25@gmail.com>
2022-08-17 01:40:30 +0100

    Continued working on the project.

---
+++ src/context/appContext.js
@@ -0,0 +1,24 @@
+import React, { useState, useReducer, useContext } from "react";
+
+const intialState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: "",
+  alertType: "",
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, setState] = useState(intialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, intialState, useAppContext };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,17 +2,19 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Logo, Formrow, Alert } from "./../components";
 import Wrapper from "./../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const intialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  showAlert: false,
 };
 
 const Register = () => {
   const [values, setValues] = useState(intialState);
+
+  const { isLoading, showAlert } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -32,7 +34,7 @@ const Register = () => {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? "Login" : "Register"}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
 
         {!values.isMember && (
           <Formrow
